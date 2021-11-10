e71c6e75b85be975d4b7c81742f2a21a389f79cb
shivampaul <pal.shivam9@gmail.com>
2021-11-10 23:39:20 +0100

    Update application.py

--- application.py
+++ application.py
@@ -3,16 +3,16 @@ from flask import jsonify
 import numpy as np
  
 application = Flask(__name__)
- 
+  
 @application.route('/')
 def hello(): 
-return 'Hello Shivam\n' 
+    return 'Hello Flask\n' 
 
 @application.route('/random/<n>')
 def randomvalues(n): 
-values = np.random.randint(0,10,int(n)) 
-result = {'values' : values.tolist()} 
-return jsonify(result) 
+    values = np.random.randint(0,10,int(n)) 
+    result = {'values' : values.tolist()} 
+    return jsonify(result) 
 
 if __name__ == '__main__': 
-application.run(debug=True) 
+      application.run(debug=True) 
