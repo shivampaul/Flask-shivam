aa919bc3d09b191da30a21e172ad375334855b25
shivampaul <pal.shivam9@gmail.com>
2021-11-13 01:37:00 +0100

    Update application.py

--- application.py
+++ application.py
@@ -6,7 +6,7 @@ application = Flask(__name__)
   
 @application.route('/')
 def hello(): 
-    return 'Hello Flask\n' 
+    return 'Hello Shivam Pal\n' 
 
 @application.route('/random/<n>')
 def randomvalues(n): 
